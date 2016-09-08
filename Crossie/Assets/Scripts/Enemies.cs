4b160b1c2eee60919d44b9780a559d3e32735f05
pratik <pratik@chiselcut.in>
2016-09-08 19:08:45 +0530

    added enemy script

---
+++ Crossie/Assets/Scripts/Enemies.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemies : MonoBehaviour
+{
+    public float enemySpeed;
+
+    void Update()
+    {
+        Movement(enemySpeed);
+    }
+
+    void Movement(float speed)
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    int Direction()
+    {
+        return 0;
+    }
+}
